20dee36b924fe47fd91f2c536d8019d63d09f2f5
S Anand <root.node@gmail.com>
2020-12-01 11:09:16 +0530

    BUG: quote pip install dependency with >

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # Add guide-specific dependencies
 conda install -y -q -c r r-ggplot2 r-rmarkdown
-pip install --upgrade openpyxl>=3.0.4 markdown-customblocks mdx_truly_sane_lists
+pip install --upgrade 'openpyxl>=3.0.4' markdown-customblocks mdx_truly_sane_lists
 
 # Set up apps
 python modelhandler/iris.py
