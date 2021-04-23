4c10356420b3281237ee27b578f6918ad818811e
S Anand <root.node@gmail.com>
2021-04-23 11:38:46 +0530

    ENH: Add UIFactory component editor

--- setup.sh
+++ setup.sh
@@ -3,4 +3,11 @@ conda install -y -q -c r rpy2 r-ggplot2 r-rmarkdown
 pip install 'openpyxl>=3.0.4' markdown-customblocks mdx_truly_sane_lists
 
 # Set up apps
+# -----------
+
+# Build the iris model one-time on install
 python modelhandler/iris.py
+
+# Brackets indicate that we run in a subshell. So after the subshell exits,
+# the current directory is retained.
+(cd uifactory/editor/ && npm install)
