9e8188785aaff8fd188e6c7cf48eeb44bc4cf6e2
Samarth <samarthg@umich.edu>
2021-05-07 14:35:58 +0530

    add mutation observer to sync changes from preview window to properties editor

--- uifactory/configure/script.js
+++ uifactory/configure/script.js
@@ -16,6 +16,7 @@ async function setup() {
   document.querySelector('bs5-tree').setAttribute('tree', JSON.stringify(collections))
 }
 setup()
+const observer = new MutationObserver(observerHandler)
 const camelize = s => s.replace(/-./g, x => x.toUpperCase()[1])
 const kebabize = str => {
   return str.split('').map((letter, idx) => {
@@ -25,7 +26,17 @@ const kebabize = str => {
   }).join('');
 }
 
-function updateCodePreview(prop, value) {
+function observerHandler(mutationRecords) {
+  const ignoreAttributes = ['class', 'style']
+  const props = {}
+  mutationRecords.forEach(record => {
+    if(record.type === 'attributes' && !ignoreAttributes.includes(record.attributeName)) {
+      props[record.attributeName] = record.target.getAttribute(record.attributeName)
+    }
+  })
+  updateCodePreview(props, true)
+}
+function updateCodePreview(props, fromPreview) {
   const iframe = document.querySelector('iframe')
   const componentName = iframe.dataset.component
   const component = iframe.contentWindow.document.querySelector(componentName)
@@ -33,12 +44,17 @@ function updateCodePreview(prop, value) {
   const defaultProperties = iframe.contentWindow.uifactory.components[componentName].properties
   delete properties.$target
   const originalNode = component.__originalNode.cloneNode(true)
-  if(prop !== undefined) {
-    component.setAttribute(prop, value)
-    properties[camelize(prop)] = value
+  if(props !== undefined) {
+    Object.keys(props).forEach(prop => {
+      if(!fromPreview) {
+        component.setAttribute(prop, props[prop])
+      } else {
+        document.querySelector(`component-properties [name="${prop}"]`).value = props[prop]
+      }
+      properties[camelize(prop)] = props[prop]
+    })
   }
   Object.keys(properties).forEach(key => {
-    // originalNode.setAttribute(kebabize(key), properties[key])
     const kebabKey = kebabize(key)
     const p = defaultProperties.find(p => p.name === kebabKey)
     if('value' in p && properties[key] != p.value) {
@@ -51,7 +67,7 @@ document.body.addEventListener('section-resized', function(e) {
   if(!e.target.matches('resizable-pane > div')) return
   document.querySelector('monaco-editor').editor.layout()
 })
-document.body.addEventListener('click', async function(e) {
+document.body.addEventListener('click', function(e) {
   if(!e.target.matches('body > div > bs5-tree li')) return
   e.preventDefault()
   const active = document.querySelector('body > div > bs5-tree .active')
@@ -59,7 +75,6 @@ document.body.addEventListener('click', async function(e) {
     active.classList.remove('active')
   }
   e.target.classList.add('active')
-
   const collectionName = e.target.closest('details').querySelector('summary').textContent.trim()
   const collection = document.querySelector(`template[name=${collectionName}]`).content
   const dependencies = collection.querySelector('template[dependencies]')
@@ -81,6 +96,12 @@ document.body.addEventListener('click', async function(e) {
   document.querySelector('monaco-editor').editor.getModel().setValue(componentSource.outerHTML)
   iframe.onload = function(e) {
     document.querySelector('component-properties').properties = e.target.contentWindow.uifactory.components[doc.body.firstElementChild.tagName.toLowerCase()].properties;
+    observer.observe(e.target.contentWindow.document.body.firstElementChild,
+      {
+        attributes: true,
+        // childList: true,
+        // subtree: true
+      })
     updateCodePreview()
   }
 })
@@ -98,7 +119,9 @@ document.body.addEventListener('click', function(e) {
 })
 document.body.addEventListener('change', function(e) {
   if(!(e.target.matches('component-properties input') || e.target.matches('component-properties toggle-switch'))) return
-  updateCodePreview(e.target.getAttribute('name'), e.target.value)
+  const props = {}
+  props[e.target.getAttribute('name')] = e.target.value
+  updateCodePreview(props)
 })
 
 document.body.addEventListener('editor-loaded', function(e) {
