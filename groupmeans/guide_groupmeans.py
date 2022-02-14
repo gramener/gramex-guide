25d76e9a6044bbe9c59a6dae884cefb1c64d6ac7
S Anand <root.node@gmail.com>
2022-02-14 16:46:18 +0530

    BUG: Add GroupMeans data to LFS instead of download from Seafile

--- groupmeans/guide_groupmeans.py
+++ groupmeans/guide_groupmeans.py
@@ -19,23 +19,5 @@ def autolyse(data, handler):
         quantile={'type': float, 'default': .95},
         minsize={'type': float, 'default': None},
         weight={'type': float, 'default': None})
-    x = gramex.ml.groupmeans(data, args.groups, args.numbers,
-                             args.cutoff, args.quantile, args.weight)
-    for col in x.select_dtypes(include='object'):
-        x[col] = x[col].astype('str')
-    return x
-
-
-@tornado.gen.coroutine
-def autolyse_download(handler):
-    '''Downloads a file from a url specified in query-parameter'''
-    _file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'nas.csv')
-    if os.path.exists(_file) and os.path.isfile(_file):
-        return 'File Exists'
-    url = 'https://cloud.gramener.com/f/c43f815486e648f482c7/?dl=1'
-    r = requests.get(url, stream=True)
-    with open(_file, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
-    return 'Downloaded'
+    return gramex.ml.groupmeans(data, args.groups, args.numbers,
+                                args.cutoff, args.quantile, args.weight)
