4a135482a5e65647e820655b27659f969967a35d
S Anand <root.node@gmail.com>
2022-05-28 06:50:59 +0530

    BUG: Remove redundant UIFactory editor installation
    The UIFactory editor has moved into the Gramex IDE as
    configuration editor

--- setup.sh
+++ setup.sh
@@ -7,7 +7,3 @@ pip install boto3 markdown-customblocks mdx_truly_sane_lists nltk textblob
 
 # Build the iris model one-time on install
 python modelhandler/iris.py
-
-# Brackets indicate that we run in a subshell. So after the subshell exits,
-# the current directory is retained.
-(cd uifactory/editor/ && npm install)
